d9beb6f51e5c3c7524cfdcabad03ccee1d10ef96
Cyril Mougel <cyril.mougel@gmail.com>
2024-02-16 18:44:41 +0100

    a version almost working with test of drunken-bishop-algorithm

--- go/drunken-bishop-algorithm/code.go
+++ go/drunken-bishop-algorithm/code.go
@@ -4,6 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 type ChessLine struct {
@@ -11,7 +14,113 @@ type ChessLine struct {
 }
 
 type ChessBoard struct {
-	Lines []ChessLine
+	Lines    []ChessLine
+	Position []int
+}
+
+func makeChessBoard(start_position []int) ChessBoard {
+	chessLines := make([]ChessLine, 9)
+	for i, _ := range chessLines {
+		line := make([]string, 17)
+		for i, _ := range line {
+			line[i] = " "
+		}
+		chessLines[i].Plot = line
+	}
+	//fmt.Println(chessLines)
+	chessLines[start_position[1]].Plot[start_position[0]] = "S"
+
+	return ChessBoard{
+		Lines:    chessLines,
+		Position: start_position,
+	}
+}
+
+func printChessBoard(chessBoard ChessBoard) {
+	fmt.Println("+---[CODINGAME]---+")
+	for i, _ := range chessBoard.Lines {
+		fmt.Printf("|")
+		for j, _ := range chessBoard.Lines[i].Plot {
+			fmt.Printf(chessBoard.Lines[i].Plot[j])
+		}
+		fmt.Printf("|\n")
+	}
+	fmt.Println("+-----------------+")
+}
+
+func decimalToBinary(num int64) []int64 {
+	binary := make([]int64, 0)
+	for num != 0 {
+		binary = append(binary, num%2)
+		num = num / 2
+	}
+	slices.Reverse(binary)
+	return binary
+}
+
+func HexTodirection(hexString string) []string {
+	i, err := strconv.ParseInt(hexString, 16, 64)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	decodedByteArray := decimalToBinary(i)
+	var result []string
+	var previous int64
+	var pair string
+
+	for i, b := range decodedByteArray {
+		if i%2 == 0 {
+			previous = b
+		} else {
+			pair = fmt.Sprintf("%d%d", previous, b)
+			result = append(result, pair)
+		}
+	}
+	return result
+}
+
+// MoveBishop move the bishop on the chessboard and mark the position with the valid value
+// [".", "o", "+", "=", "*", "B", "O", "X", "@", "%", "&", "#", "/", "^"]
+var ListOfSymbols = []string{".", "o", "+", "=", "*", "B", "O", "X", "@", "%", "&", "#", "/", "^"}
+
+// Find the next symbol to use as marker
+func NextMarker(current string) string {
+	for i, symbol := range ListOfSymbols {
+		if current == symbol {
+			return ListOfSymbols[i+1]
+		}
+	}
+	return "."
+}
+
+func MoveBishop(chessBoard *ChessBoard, directions []string) {
+	var new_position []int
+	for _, direction := range directions {
+		if direction == "11" {
+			new_position = []int{chessBoard.Position[0] - 1, chessBoard.Position[1] + 1}
+		} else if direction == "10" {
+			new_position = []int{chessBoard.Position[0] - 1, chessBoard.Position[1] - 1}
+		} else if direction == "00" {
+			new_position = []int{chessBoard.Position[0] + 1, chessBoard.Position[1] - 1}
+		} else if direction == "01" {
+			new_position = []int{chessBoard.Position[0] + 1, chessBoard.Position[1] + 1}
+		}
+		if new_position[0] > 16 {
+			new_position[0] = 16
+		}
+		if new_position[1] > 8 {
+			new_position[1] = 8
+		}
+		if new_position[0] < 0 {
+			new_position[0] = 0
+		}
+		if new_position[1] < 0 {
+			new_position[1] = 0
+		}
+		fmt.Println(new_position)
+		chessBoard.Lines[new_position[1]].Plot[new_position[0]] = NextMarker(chessBoard.Lines[new_position[1]].Plot[new_position[0]])
+		chessBoard.Position = new_position
+	}
 }
 
 func main() {
@@ -22,28 +131,11 @@ func main() {
 	fingerprint := scanner.Text()
 	_ = fingerprint // to avoid unused error
 
-	line := make([]string, 17)
-	for i, _ := range line {
-		line[i] = " "
+	chess_board := makeChessBoard([]int{8, 4})
+	for _, hex := range strings.Split(fingerprint, ":") {
+		MoveBishop(&chess_board, HexTodirection(hex))
 	}
-	fmt.Println(line)
-	chessLines := make([]ChessLine, 9)
-	for i, _ := range chessLines {
-		chessLines[i].Plot = line
-	}
-	//fmt.Println(chessLines)
-	chess_board := ChessBoard{Lines: chessLines}
-
-	fmt.Println(chess_board)
+	printChessBoard(chess_board)
 
 	// fmt.Fprintln(os.Stderr, "Debug messages...")
-	fmt.Println("+---[CODINGAME]---+")
-	for i, _ := range chess_board.Lines {
-		fmt.Printf("|")
-		for j, _ := range chess_board.Lines[i].Plot {
-			fmt.Printf(chess_board.Lines[i].Plot[j])
-		}
-		fmt.Printf("|\n")
-	}
-	fmt.Println("+-----------------+")
 }
---
+++ go/drunken-bishop-algorithm/code_test.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestHexToDirection(t *testing.T) {
+	directive := "F"
+	result := HexTodirection(directive)
+	want := []string{"11", "11"}
+	want[0] = "11"
+	want[1] = "11"
+	if reflect.DeepEqual(result, want) == false {
+		t.Errorf("Expected %v, got %v\n", want, result)
+	}
+}
+
+func TestHexToDirectionWithSeveralChar(t *testing.T) {
+	directive := "FC"
+	result := HexTodirection(directive)
+	want := []string{"11", "11", "11", "00"}
+	if reflect.DeepEqual(result, want) == false {
+		t.Errorf("Expected %v, got %v\n", want, result)
+	}
+}
+
+func TestMoveBishop11(t *testing.T) {
+	board := makeChessBoard([]int{8, 4})
+	move := []string{"11"} // move to Bottom Right
+	MoveBishop(&board, move)
+	if board.Lines[4+1].Plot[8-1] != "." {
+		t.Errorf("Expected ., got %v\n", board.Lines[4+1].Plot[8-1])
+	}
+}
+
+func TestMoveBishop10(t *testing.T) {
+	board := makeChessBoard([]int{8, 4})
+	move := []string{"10"} // move to bottom left
+	MoveBishop(&board, move)
+	if board.Lines[4-1].Plot[8-1] != "." {
+		t.Errorf("Expected ., got %v\n", board.Lines[4-1].Plot[8-1])
+	}
+}
+
+func TestMoveBishop00(t *testing.T) {
+	board := makeChessBoard([]int{8, 4})
+	move := []string{"00"} // move to top left
+	MoveBishop(&board, move)
+	if board.Lines[4-1].Plot[8+1] != "." {
+		t.Errorf("Expected ., got %v\n", board.Lines[4-1].Plot[8+1])
+	}
+}
+
+func TestMoveBishop01(t *testing.T) {
+	board := makeChessBoard([]int{8, 4})
+	move := []string{"01"} // move to top right
+	MoveBishop(&board, move)
+	if board.Lines[4+1].Plot[8+1] != "." {
+		t.Errorf("Expected ., got %v\n", board.Lines[4+1].Plot[8+1])
+	}
+}
+
+func TestMoveBishop00OnEdge(t *testing.T) {
+	board := makeChessBoard([]int{16, 4})
+	move := []string{"00"} // move to top left
+	MoveBishop(&board, move)
+	if board.Lines[4-1].Plot[16] != "." {
+		t.Errorf("Expected ., got %v\n", board.Lines[4-1].Plot[16])
+	}
+}
+
+func TestMoveBishop01OnEdge(t *testing.T) {
+	board := makeChessBoard([]int{7, 8})
+	move := []string{"01"} // move to top left
+	MoveBishop(&board, move)
+	if board.Lines[8].Plot[7+1] != "." {
+		t.Errorf("Expected ., got %v\n", board.Lines[8].Plot[7+1])
+	}
+}
+
+func TestMoveBishop11OnEdge(t *testing.T) {
+	board := makeChessBoard([]int{0, 4})
+	move := []string{"11"} // move to top left
+	MoveBishop(&board, move)
+	if board.Lines[4+1].Plot[0] != "." {
+		t.Errorf("Expected ., got %v\n", board.Lines[4+1].Plot[0])
+	}
+}
+
+func TestMoveBishop10OnEdge(t *testing.T) {
+	board := makeChessBoard([]int{2, 0})
+	move := []string{"10"} // move to top left
+	MoveBishop(&board, move)
+	if board.Lines[0].Plot[2-1] != "." {
+		t.Errorf("Expected ., got %v\n", board.Lines[0].Plot[2-1])
+	}
+}
+
+func TestMoveBishopSeveralTime(t *testing.T) {
+	board := makeChessBoard([]int{8, 4})
+	move := []string{"11"} // move to Bottom Right
+	for _, symbol := range []string{".", "o", "+", "=", "*", "B", "O", "X", "@", "%", "&", "#", "/", "^"} {
+		MoveBishop(&board, move)
+		newPosition := []int{8 - 1, 4 + 1}
+		if board.Lines[newPosition[1]].Plot[newPosition[0]] != symbol {
+			t.Errorf("Expected %v, got %v\n", symbol, board.Lines[newPosition[1]].Plot[newPosition[0]])
+		}
+
+		if board.Position[0] != newPosition[0] || board.Position[1] != newPosition[1] {
+			t.Errorf("Expected %v, got %v\n", newPosition, board.Position)
+		}
+		board.Position = []int{8, 4}
+	}
+}
