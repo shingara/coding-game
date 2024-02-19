5965c82a3205c51a0fda2be51e9458a11997d25b
Cyril Mougel <cyril.mougel@gmail.com>
2024-02-19 13:22:41 +0100

    300

--- ruby/dont-panic/mini_code.rb
+++ ruby/dont-panic/mini_code.rb
@@ -1,11 +1,9 @@
-STDOUT.sync = true
-
-nf, w, _, ef, ep, _, _, ne = gets.split(" ").collect{|i| i.to_i}
-elevators = {ef => ep}
-ne.times{e,p=gets.split(" "); elevators[e.to_i] = p.to_i}
-
-loop do
-    clone = gets.split(" ") # [$cloneFloor.to_i, $clonePos.to_i, $direction] #clone is an Array clone[0] = floor, clone[1] = pos, clone[2] = direction
-    good_direction = clone[2] == "RIGHT" ? clone[1].to_i - 1 < elevators[clone[0].to_i] : clone[1].to_i + 1 > (elevators[clone[0].to_i] || 0)
-    puts ([w-1, 0].include?(clone[1].to_i) || !good_direction) ? "BLOCK" : "WAIT"
-end
+STDOUT.sync=true
+nf,w,_,ef,ep,_,_,ne=gets.split(" ").collect{|i| i.to_i}
+es={ef => ep}
+ne.times{e,p=gets.split(" ");es[e.to_i]=p.to_i}
+loop{
+c=gets.split(" ")
+gd= c[2] == "RIGHT" ? c[1].to_i - 1 < es[c[0].to_i] : c[1].to_i + 1 > (es[c[0].to_i] || 0)
+puts ([w-1, 0].include?(c[1].to_i) || !gd)?"BLOCK":"WAIT"
+}
