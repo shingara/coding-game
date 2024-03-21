7d5960d177b4ddacbbf2df78ae9ce751200b102c
Cyril Mougel <cyril.mougel@gmail.com>
2024-03-21 15:53:28 +0100

    [ruby][mars-lander-2] 100% success

--- ruby/mars-lander-2/code.rb
+++ ruby/mars-lander-2/code.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 STDOUT.sync = true # DO NOT REMOVE
 
+# Arbitrary values of max horizontal speed
+MAX_DX = 20
+MAX_DY = 40
+
 class Land
   def initialize(x, y)
     @x = x
@@ -64,18 +68,22 @@ class Lander
     ]
   end
 
-  def in_wrong_direction(zone)
-    (x < zone.land1.x && hs <= 0 ) ||
-      (x > zone.land2.x && hs >= 0)
+  def in_wrong_direction?(zone)
+    (x < zone.land1.x && hs < 0 ) ||
+      (x > zone.land2.x && hs > 0)
   end
 
   def min_distance_to_land(zone)
     [(zone.land1.x - x).abs,
-      (zone.land2.x - x).abs].min
+     (zone.land2.x - x).abs].min + (zone.land1.x - zone.land2.x).abs
+  end
+
+  def min_height_to_land(zone)
+    (zone.land1.y - y).abs
   end
 
   def time_to_go_horizontal(zone)
-    if in_wrong_direction(zone)
+    if in_wrong_direction?(zone) || hs == 0
       1000
     else
       # calcul distance to x / horizontal speed
@@ -83,8 +91,101 @@ class Lander
     end
   end
 
+  def time_to_go_vertical(zone)
+    min_height_to_land(zone) / vs.abs.to_f
+  end
+
+  def position_to_land(time)
+    x + time*hs.abs
+  end
+
+  def position_to_land_x(zone)
+    min_time_to_go(zone)*hs + x
+  end
+
+  def position_to_land_y(zone)
+    min_time_to_go(zone)*vs + y
+  end
+
+  def min_time_to_go(zone)
+    [time_to_go_horizontal(zone), time_to_go_vertical(zone)].min
+  end
+
+  def get_in_zone?(zone)
+    ((zone.land1.x + 1)..(zone.land2.x - 1)).include?(min_time_to_go(zone)*hs + x)
+  end
+
   def go_to(zone)
-    [r, t].join(' ')
+    warn "zone to land : #{zone.land1}, #{zone.land2}"
+    warn "time_to_go_horizontal: #{time_to_go_horizontal(zone)}"
+    warn "time_to_go_vertical: #{time_to_go_vertical(zone)}"
+    warn "position_to_land_x: #{position_to_land_x(zone)}"
+    warn "get_in_zone: #{get_in_zone?(zone)}"
+
+    # not on top of the zone
+    unless on_top_of_zone?(zone)
+      if in_wrong_direction?(zone) || go_to_fast_horizontaly?
+        return [angle_to_slow, 4].join(' ')
+      elsif go_to_slow_horizontaly?
+        return [angle_to_aim(zone), 4].join(' ')
+      else
+        return [0, power_to_hover].join(' ')
+      end
+    else
+      if close_to_land?(zone)
+        return [0, 3].join(' ')
+      elsif safe_speed
+        return [0, 2].join(' ')
+      else
+        return [angle_to_slow, 4].join(' ')
+      end
+    end
+  end
+
+  def safe_speed
+    hs.abs < MAX_DX - 5 && vs.abs < MAX_DY - 5
+  end
+
+  def power_to_hover
+    warn "power to hover #{vs.abs}"
+    vs >= 0 ? 3 : 4
+  end
+
+  def close_to_land?(zone)
+    y < zone.land1.y + 100
+  end
+
+  ##
+  # Check if the lander is on top of the zone
+  def on_top_of_zone?(zone)
+    (zone.land1.x..zone.land2.x).include?(x)
+  end
+
+  def go_to_fast_horizontaly?
+    hs.abs > 4*MAX_DX
+  end
+
+  def go_to_slow_horizontaly?
+    hs.abs < 2*MAX_DX
+  end
+
+  def angle_to_aim(zone)
+    angle = (
+      (Math.cos(GRAVITY / 4) * 180) / Math::PI
+    ).ceil
+    if x < zone.land1.x
+      -angle
+    else
+      angle
+    end
+  end
+
+  def angle_to_slow
+    warn "hs**2 + vs**2 : #{hs**2 + vs**2}"
+    warn "sqrt hs**2 + vs**2 : #{Math.sqrt(hs**2 + vs**2)}"
+    (
+                Math.asin(hs / Math.sqrt(hs**2 + vs**2).to_f) * 180 / Math::PI
+    ).to_i
   end
 
 end
@@ -102,7 +203,7 @@ unless ENV['TEST']
   end
 
   lands_to_land = ZoneToLand.process(lands)
-  warn lands_to_land
+  warn "lands to land : #{lands_to_land}"
 
   loop do
     # HS: the horizontal speed (in m/s), can be negative.
@@ -112,7 +213,7 @@ unless ENV['TEST']
     # P: the thrust power (0 to 4).
     $X, $Y, $HS, $VS, $F, $R, $P = gets.split(" ").collect {|x| x.to_i}
     lander = Lander.new($X, $Y, $HS, $VS, $F, $R, $P)
-    warn lander.next_position
+    warn "next position #{lander.next_position}"
     puts lander.go_to(lands_to_land)
     #puts "0 0" # R P. R is the desired rotation angle. P is the desired thrust power.
   end
--- ruby/mars-lander-2/test_code.rb
+++ ruby/mars-lander-2/test_code.rb
@@ -22,15 +22,15 @@ RSpec.describe Lander do
   let(:lander) { Lander.new(0, 0, 0, 0, 0, 0, 0) }
   let(:landing_zone) { Zone.new(Land.new(2, 3), Land.new(4, 3)) }
 
-  describe '#in_wrong_direction' do
+  describe '#in_wrong_direction?' do
     describe 'lander not move' do
       describe 'outside the landing zone' do
         before do
           lander.x = 0
-          lander.hs = 0
+          lander.hs = -1
         end
         it 'returns true' do
-          expect(lander.in_wrong_direction(landing_zone)).to be true
+          expect(lander.in_wrong_direction?(landing_zone)).to be true
         end
       end
 
@@ -39,7 +39,7 @@ RSpec.describe Lander do
           lander.x = 3
         end
         it 'returns true' do
-          expect(lander.in_wrong_direction(landing_zone)).to be false
+          expect(lander.in_wrong_direction?(landing_zone)).to be false
         end
       end
     end
@@ -50,7 +50,7 @@ RSpec.describe Lander do
         lander.hs = 2
       end
       it 'returns false' do
-        expect(lander.in_wrong_direction(landing_zone)).to be false
+        expect(lander.in_wrong_direction?(landing_zone)).to be false
       end
     end
 
@@ -60,7 +60,7 @@ RSpec.describe Lander do
         lander.hs = -1
       end
       it 'returns true' do
-        expect(lander.in_wrong_direction(landing_zone)).to be true
+        expect(lander.in_wrong_direction?(landing_zone)).to be true
       end
     end
   end
@@ -82,9 +82,9 @@ RSpec.describe Lander do
     context 'lander go to right direction' do
       it 'return time to go to the landing zone' do
         {
-          [0, 2] => 1,
-          [0, 4] => 0.5,
-          [-10, 2] => 6,
+          [0, 2] => 2,
+          [0, 4] => 1,
+          [-10, 2] => 7,
         }.each do |d, r|
           lander.x = d[0]
           lander.hs = d[1]
@@ -102,13 +102,170 @@ RSpec.describe Lander do
         expect(lander.time_to_go_horizontal(landing_zone)).to be == 1000
       end
     end
+
+  end
+
+  describe '#time_to_go_vertical' do
+    before do
+      lander.x = -10
+      lander.hs = 2
+    end
+
+    context 'lander go to wrong direction' do
+      before do
+        lander.y = 10
+        lander.vs = 5
+      end
+
+      it 'returns 1000' do
+        expect(lander.time_to_go_vertical(landing_zone)).to be == (10-3).to_f/5
+      end
+    end
+  end
+
+  describe "#position_to_land_x" do
+    before do
+      lander.x = 10
+      lander.hs = 2
+      expect(lander).to receive(:min_time_to_go).with(landing_zone).and_return(3)
+    end
+    it 'returns 10' do
+      expect(lander.position_to_land_x(landing_zone)).to be == 10 + 3*2
+    end
+  end
+
+  describe '#min_time_to_go' do
+    context "same time both" do
+      before do
+        lander.x = 10
+        lander.y = 10
+        lander.hs = 2
+        lander.vs = 2
+      end
+      # Go do 2-3 / 3
+      # time to go down (y - ly) / vs => (10 - 3) / 2
+      # time to go right (x - lx) / hs => (10 - 3) / 2
+      # get the lowest
+      it 'returns 1000' do
+        expect(lander.min_time_to_go(landing_zone)).to be == 7/2.to_f
+      end
+    end
+  end
+
+  describe '#get_in_zone?' do
+    before do
+      lander.x = 10
+      lander.y = 10
+      lander.hs = 2
+      lander.vs = 2
+    end
+    it 'returns false' do
+      expect(lander.get_in_zone?(landing_zone)).to be false
+    end
   end
 
   describe '#go_to' do
     describe 'landing_zone at right' do
       it 'go to to' do
-        expect(lander.go_to(landing_zone)).to be == '0 3'
+        expect(lander.go_to(landing_zone)).to be == '0 4'
       end
     end
+  end
+
+  describe '#on_top_of_zone' do
+    context "on top of the zone" do
+      before do
+        lander.x = rand(landing_zone.land1.x..landing_zone.land2.x)
+      end
+      it 'return true' do
+        expect(lander.on_top_of_zone?(landing_zone)).to be true
+      end
+    end
+    context 'right of the zone' do
+      before do
+        lander.x = landing_zone.land2.x + 1
+      end
+      it 'return false' do
+        expect(lander.on_top_of_zone?(landing_zone)).to be false
+      end
+    end
+    context 'left of the zone' do
+      before do
+        lander.x = landing_zone.land1.x - 1
+      end
+      it 'return false' do
+        expect(lander.on_top_of_zone?(landing_zone)).to be false
+      end
+    end
+  end
+
+
+  describe '#go_to_fast_horizontaly?' do
+    context 'with a high speed' do
+      before do
+        lander.hs = 5*MAX_DX
+      end
+      it' return true' do
+        expect(lander.go_to_fast_horizontaly?).to be true
+      end
+    end
+    context 'with a low speed' do
+      before do
+        lander.hs = 2*MAX_DX
+      end
+      it' return false' do
+        expect(lander.go_to_fast_horizontaly?).to be false
+      end
+    end
+  end
+
+  describe '#go_to_slow_horizontaly?' do
+    context 'with a high speed' do
+      before do
+        lander.hs = 3*MAX_DX
+      end
+      it' return false' do
+        expect(lander.go_to_slow_horizontaly?).to be false
+      end
+    end
+    context 'with a low speed' do
+      before do
+        lander.hs = 1*MAX_DX
+      end
+      it' return true' do
+        expect(lander.go_to_slow_horizontaly?).to be true
+      end
+    end
+  end
+
+  describe '#angle_to_aim(zone)' do
+    context 'with a zone at right' do
+      before do
+        lander.x = landing_zone.land1.x - 1
+      end
+      it 'return speed to aim to right' do
+        expect(lander.angle_to_aim(landing_zone)).to be == -34
+      end
+    end
+    context 'with a zone at left' do
+      before do
+        lander.x = landing_zone.land2.x + 1
+      end
+      it 'return speed to aim to left' do
+        expect(lander.angle_to_aim(landing_zone)).to be == 34
+      end
+    end
+  end
+
+  describe '#angle_to_slow' do
+    before do
+      lander.hs = 2
+      lander.vs = 3
+    end
+
+    it 'return the angle to slow' do
+      expect(lander.angle_to_slow).to be == 33
+    end
+
   end
 end
